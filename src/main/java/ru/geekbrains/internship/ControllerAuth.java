d66444a5bf74d4b12c9bc3ed495549fa3a9ae88d
Dmitry Grinshteyn <dgrin2005@mail.ru>
2017-12-19 15:18:41 +0600

    Добавлен контроллер формы авторизации

--- src/main/java/ru/geekbrains/internship/ControllerAuth.java
+++ src/main/java/ru/geekbrains/internship/ControllerAuth.java
@@ -1,4 +1,36 @@
 package ru.geekbrains.internship;
 
 public class ControllerAuth {
+
+    private StartWindow mainApp;
+    private ConnectionDB connDB;
+
+    public void setMainApp(StartWindow mainApp) {
+        this.mainApp = mainApp;
+    }
+
+    public void setDBApp(ConnectionDB connDB) {
+        this.connDB = connDB;
+    }
+
+    public void pressOkButton() {
+        try {
+            if (connDB.checkAuthorization()) {
+                new DesktopUI(mainApp, connDB);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void pressCancelButton() {
+        try {
+            System.exit(0);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
