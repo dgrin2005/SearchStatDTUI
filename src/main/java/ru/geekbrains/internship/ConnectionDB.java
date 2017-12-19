e462f30cabaabed72b24ea53a0cc3a1bab8b11fd
Dmitry Grinshteyn <dgrin2005@mail.ru>
2017-12-19 15:18:26 +0600

    Добавлен класс по работе с БД

--- src/main/java/ru/geekbrains/internship/ConnectionDB.java
+++ src/main/java/ru/geekbrains/internship/ConnectionDB.java
@@ -1,4 +1,39 @@
 package ru.geekbrains.internship;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class ConnectionDB {
+
+    public boolean checkAuthorization() {
+        return true;
+    }
+
+    public ObservableList getTotalStatisticsList() {
+        ObservableList<TotalStatistics> totalStatisticsList = FXCollections.observableArrayList(
+                new TotalStatistics("Путин",5000),
+                new TotalStatistics("Медведев",4000),
+                new TotalStatistics("Навальный",3000));
+        return totalStatisticsList;
+    }
+
+    public ObservableList getDailyStatisticsList() {
+        ObservableList<DailyStatistics> dailyStatisticsList = FXCollections.observableArrayList(
+                new DailyStatistics("01-12-2017", 50),
+                new DailyStatistics("02-12-2017",40),
+                new DailyStatistics("03-12-2017",30));
+        return dailyStatisticsList;
+    }
+
+    public ObservableList getSites() {
+        ObservableList<String> sites = FXCollections.observableArrayList(
+                "lenta.ru","rbc.ru","rambler.ru");
+        return sites;
+    }
+    public ObservableList getNames() {
+        ObservableList<String> names = FXCollections.observableArrayList(
+                "Путин","Медведев","Навальный");
+        return names;
+    }
+
 }
