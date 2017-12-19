63e1b794c4f255de09860df46b783c2c2b6575e9
Dmitry Grinshteyn <dgrin2005@mail.ru>
2017-12-19 15:18:57 +0600

    Добавлен контроллер формы стартового окна

--- src/main/java/ru/geekbrains/internship/ControllerStart.java
+++ src/main/java/ru/geekbrains/internship/ControllerStart.java
@@ -1,4 +1,24 @@
 package ru.geekbrains.internship;
 
 public class ControllerStart {
+
+    private StartWindow mainApp;
+    private ConnectionDB connDB;
+
+    public void setMainApp(StartWindow mainApp) {
+        this.mainApp = mainApp;
+    }
+
+    public void setDBApp(ConnectionDB connDB) {
+        this.connDB = connDB;
+    }
+
+    public void pressStartButton() {
+        try {
+            new AuthorizationWindow(mainApp, connDB);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
