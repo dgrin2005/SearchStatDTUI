30345aa90c6058e54a19d160cbb627d1bc31f086
Dmitry Grinshteyn <dgrin2005@mail.ru>
2018-01-05 18:00:26 +0600

    Доюавлены команды для паттерна "Команда"

---
+++ src/main/java/ru/geekbrains/internship/GetTotalStatisticsCommand.java
@@ -0,0 +1,35 @@
+package ru.geekbrains.internship;
+
+import javafx.geometry.Side;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableView;
+
+public class GetTotalStatisticsList implements Command {
+    private ChoiceBox<String> totalStatisticsSite;
+    private TableView<TotalStatistics> totalStatisticsTable;
+    private StartWindow mainApp;
+    private PieChart totalStatisticsChart;
+
+    public GetTotalStatisticsList(StartWindow mainApp, ChoiceBox<String> totalStatisticsSite, TableView<TotalStatistics> totalStatisticsTable, PieChart totalStatisticsChart) {
+        this.mainApp = mainApp;
+        this.totalStatisticsSite = totalStatisticsSite;
+        this.totalStatisticsTable = totalStatisticsTable;
+        this.totalStatisticsChart = totalStatisticsChart;
+    }
+
+    @Override
+    public void execute() {
+        if (totalStatisticsSite.getValue() != null) {
+            totalStatisticsTable.setItems(mainApp.getRequestDB().getTotalStatisticsList(mainApp.getDBStringURL(),
+                    totalStatisticsSite.getValue()));
+            totalStatisticsChart.setData(mainApp.getRequestDB().getTotalStatisticsChartData());
+            totalStatisticsChart.setLabelLineLength(10);
+            totalStatisticsChart.setLegendSide(Side.LEFT);
+        } else {
+            new AlertHandler(Alert.AlertType.WARNING, "Не заполнен параметр",
+                    "Внимание!", "Необходимо выбрать сайт");
+        }
+    }
+}
